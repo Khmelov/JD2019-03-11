7ccd73372ffb49156c7c7170f5ca7510d4caeaf1
yourGitHubusername <Arol_33@mail.ru  git config --global user.name Katyaaaa>
2019-05-23 21:46:56 +0300

    jd03_04

--- src/by/it/eslaikouskaya/jd03_04/java/FrontController.java
+++ src/by/it/eslaikouskaya/jd03_04/java/FrontController.java
@@ -7,19 +7,33 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class FrontController extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		process(req, resp);
-	}
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        process(req, resp);
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		process(req, resp);
-	}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        process(req, resp);
+    }
 
-
-	private void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("I work");
-	}
+    private void process(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        try {
+            Cmd cmd = Actions.defineCommand(req);
+            Cmd next = cmd.execute(req);
+            if (next == cmd || next == null) {
+                getServletContext().
+                        getRequestDispatcher(cmd.getJsp()).
+                        forward(req, resp);
+            } else {
+                resp.getWriter().print(next + " redirect");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
