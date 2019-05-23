9622aea5b83b7abb4f28e062d535cda52e99952f
SergPileiko <Sr.pileiko@gmail.com>
2019-05-23 09:14:50 +0300

    pileiko 03_01 change

--- src/by/it/pileiko/jd03_01/B_ShowUsers.java
+++ src/by/it/pileiko/jd03_01/B_ShowUsers.java
@@ -1,4 +1,24 @@
 package by.it.pileiko.jd03_01;
 
+import java.sql.*;
+
 public class B_ShowUsers {
+
+    public static void main(String[] args) throws SQLException {
+
+        try (Connection connection= DriverManager.getConnection(ConnectPath.URL, ConnectPath.USER, ConnectPath.PASSWORD);
+             Statement statement = connection.createStatement()
+        ) {
+
+            ResultSet resultSet = statement.executeQuery( "SELECT * FROM `pileiko`.`users`;");
+
+            while (resultSet.next()){
+                String out = resultSet.getString("Login") + ", " + resultSet.getString("roles_ID");
+                System.out.println(out);
+
+            }
+
+        }
+
+    }
 }
